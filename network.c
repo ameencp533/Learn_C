e05a75bd416a91d53c53cceca8f5b7f661442289
ipcore <ipcore@sifycorp.com>
2025-05-23 00:03:42 +0530

    createDevice and Display device added

--- network.c
+++ network.c
@@ -3,7 +3,58 @@
 #include <string.h>
 
 
+struct device {
+    int device_id;
+    char ip[50];
+    char hostname[50];
+    struct device * next;
+
+};
+
+
+struct device * create_device(int id , char * ip_ptr , char * hn_ptr){
+
+        struct device * current_device = (struct device *) malloc(sizeof(struct device ));
+
+        current_device->device_id=id;
+        strcpy(current_device->ip, *ip_ptr);
+        strcpy(current_device->hostname ,*hn_ptr) ;
+
+        current_device->next=NULL;
+
+        return current_device;
+        
+        
+}
+
+void display_connection(struct device ** current_device_ptr){
+
+    struct device * temp = *current_device_ptr;
+    while(temp!=NULL){
+        printf("%s ",temp->hostname);
+        if (temp->next!=NULL){
+            printf("==> ");
+        }
+        temp=temp->next;
+    }
+
+    printf ("\n");
+
+
+}
+
+
+
+
 int main(){
 
-    printf("hello world");
+    char ip1[] ={"100.65.212.6"};
+    char hostname1[]= {"R1"};
+    struct device * device1 = create_device (1,&ip1,&hostname1);
+
+    display_connection(&device1);
+
+    free(device1);
+    
+    return 0;
 }
